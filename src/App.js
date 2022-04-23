7e45f0e4a65c91844c8f6c71728e0a0b2dffad36
Abishek Agarwal <disk.abishek@gmail.com>
2022-04-23 18:52:22 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <Router>
@@ -9,7 +14,10 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' exact /> 
+        <Route path='/' exact element={<Home />} /> 
+        <Route path='/services' exact element={<Services />} /> 
+        <Route path='/products' exact element={<Products />} /> 
+        <Route path='/signup' exact element={<SignUp />} /> 
       </Routes>
 
     </Router>
